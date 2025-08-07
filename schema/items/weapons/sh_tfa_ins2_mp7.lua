5a094f67b3fb5423703c2f00938fa1ad2d1c7c2b
Heartex21 <burakcankaratas78@gmail.com>
2025-08-07 18:38:17 +0300

    BtL150 - Weapon changes

--- schema/items/weapons/sh_tfa_ins2_mp7.lua
+++ schema/items/weapons/sh_tfa_ins2_mp7.lua
@@ -1,7 +1,7 @@
 ITEM.name = "C-7 Compact SMG"
 ITEM.description = "A  compact, fully automatic sub-machine gun manufactured by the Combine. It is a very reliable weapon with a high rate of fire, but it is not very accurate at long ranges."
-ITEM.model = "models/weapons/w_smg1.mdl"
-ITEM.class = "tfa_projecthl2_smg"
+ITEM.model = "models/weapons/tfa_ins2/w_mp7.mdl"
+ITEM.class = "tfa_ins2_mp7"
 ITEM.weaponCategory = "primary"
 ITEM.classes = {CLASS_EMP, CLASS_EOW}
 ITEM.flag = "V"
