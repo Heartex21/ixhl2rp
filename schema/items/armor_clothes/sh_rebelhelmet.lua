6790bbd69ba0e55d24c3ec32cca5f897a74ebdc8
Heartex21 <burakcankaratas78@gmail.com>
2025-08-07 18:44:47 +0300

    BtL151 - Rebel Helmet Changes

--- schema/items/armor_clothes/sh_rebelhelmet.lua
+++ schema/items/armor_clothes/sh_rebelhelmet.lua
@@ -1,5 +1,6 @@
 ITEM.name = "L2 Helmet"
 ITEM.description = "A resistance helmet,crafted from scrap metal and other materials. It provides decent protection against small arms fire."
+ITEM.model = Model("models/willardnetworks/clothingitems/head_helmet.mdl")
 ITEM.category = "Outfit"
 ITEM.outfitCategory = "hat"
 ITEM.bodyGroups = {
@@ -7,14 +8,25 @@ ITEM.bodyGroups = {
 }
 
 function ITEM:OnEquipped()
-    -- Ensure the armor value is not reset unnecessarily
-    if not self.armor then
-        self.armor = self.maxArmor
-    end
+	self.player:SetArmor(self:GetData("armor", self.maxArmor))
 end
 
 function ITEM:OnUnequipped()
+	self:SetData("armor", math.Clamp(self.player:Armor(), 0, self.maxArmor))
+	self.player:SetArmor(0)
     self.bodyGroups = {
         ["head"] = 0
     }
+end
+
+function ITEM:OnLoadout()
+	if (self:GetData("equip")) then
+		self.player:SetArmor(self:GetData("armor", self.maxArmor))
+	end
+end
+
+function ITEM:OnSave()
+	if (self:GetData("equip")) then
+		self:SetData("armor", math.Clamp(self.player:Armor(), 0, self.maxArmor))
+	end
 end
