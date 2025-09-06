f396fe43258bb5c20573b9bb2a99f0699d158ebc
Heartex21 <burakcankaratas78@gmail.com>
2025-09-06 21:38:05 +0300

    BtL178 - Custom Guns finally optimized

--- schema/items/weapons/sh_tfa_ins2_mp7.lua
+++ schema/items/weapons/sh_tfa_ins2_mp7.lua
@@ -1,6 +1,6 @@
 ITEM.name = "C-7 Compact SMG"
 ITEM.description = "A  compact, fully automatic sub-machine gun manufactured by the Combine. It is a very reliable weapon with a high rate of fire, but it is not very accurate at long ranges."
-ITEM.model = "models/weapons/w_mp7.mdl"
+ITEM.model = "models/weapons/tfa_ins2/w_mp7.mdl"
 ITEM.class = "tfa_ins2_mp7"
 ITEM.weaponCategory = "primary"
 ITEM.classes = {CLASS_EMP, CLASS_EOW}
