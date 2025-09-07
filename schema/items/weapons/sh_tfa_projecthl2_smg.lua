56027e65b658fab7217ca4433620c23376770379
Heartex21 <burakcankaratas78@gmail.com>
2025-09-07 19:36:53 +0300

    BtL185 - Finalize Additions, Add combine pistol and smg

--- schema/items/weapons/sh_tfa_projecthl2_smg.lua
+++ schema/items/weapons/sh_tfa_projecthl2_smg.lua
@@ -1,6 +1,6 @@
 ITEM.name = "MP7 Submachine Gun"
 ITEM.description = "A  compact, fully automatic sub-machine gun manufactured by the Combine. It is a very reliable weapon with a high rate of fire and it is practically accurate at long ranges."
-ITEM.model = "models/weapons/w_iiopnsmg1.mdl"
+ITEM.model = "models/weapons/c_iiopnsmg1.mdl"
 ITEM.class = "tfa_projecthl2_smg"
 ITEM.weaponCategory = "primary"
 ITEM.classes = {CLASS_EMP, CLASS_EOW}
@@ -10,5 +10,5 @@ ITEM.height = 2
 ITEM.iconCam = {
 	ang	= Angle(-0.020070368424058, 270.40155029297, 0),
 	fov	= 7.2253324508038,
-	pos	= Vector(0, 200, -1)
+	pos	= Vector(-22, 200, -5)
 }
---
+++ schema/items/weapons/sh_tfa_projecthl2_usp.lua
@@ -0,0 +1,12 @@
+ITEM.name = "USP Match"
+ITEM.description = "Manufactured by the Combine, the USP Match is a semi pistol chambered in 9x19 mm Parabellum Ammunition. It is a reliable sidearm with moderate damage and accuracy."
+ITEM.model = ("models/weapons/w_pistol.mdl")
+ITEM.class = "tfa_projecthl2_usp"
+ITEM.weaponCategory = "sidearm"
+ITEM.width = 2
+ITEM.height = 1
+ITEM.iconCam = {
+	ang	= Angle(-0.020070368424058, 270.40155029297, 0),
+	fov	= 7.2253324508038,
+	pos	= Vector(-2.5, 200, 0)
+}
